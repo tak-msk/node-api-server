c3582c902ba29eee5769a8162c33953d31fb15d0
tak <tak.msk2580@gmail.com>
2016-04-10 16:49:49 +0900

    get mongodb works in vagrant

--- Vagrantfile
+++ Vagrantfile
@@ -14,6 +14,9 @@ Vagrant.configure(2) do |config|
   # boxes at https://atlas.hashicorp.com/search.
   config.vm.box = "ubuntu1404"
 
+  # SET Locale('C' is default of it)
+  ENV['LC_ALL']="C"
+
   # Disable automatic box update checking. If you disable this, then
   # boxes will only be checked for updates when the user runs
   # `vagrant box outdated`. This is not recommended.
--- provision.sh
+++ provision.sh
@@ -1,6 +1,12 @@
 # define a function that checks if a file exists
 exists() { type -t "$1" > /dev/null 2>&1; }
 
+# Import the public key used by the package management system
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+
+# Create a list file for MongoDB
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
 # update packages
 sudo apt-get update
 
@@ -11,6 +17,12 @@ sudo apt-get install -y build-essential curl git vim
 if ! exists node; then
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 sudo apt-get install -y nodejs
+fi
+
+# install mongodb (v3.2.4)
+if ! exists mongo; then
+sudo apt-get install -y mongodb-org=3.2.4 mongodb-org-server=3.2.4 mongodb-org-shell=3.2.4 mongodb-org-mongos=3.2.4 mongodb-org-tools=3.2.4
+sudo mkdir -p /data/db
 fi
 
 # install pm2 (node process manager)
